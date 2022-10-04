77040751c48590a9e40757b9101f65e7baf36050
soooyaya <hsoo0531@naver.com>
2022-10-04 17:34:42 +0900

    05

--- main.c
+++ main.c
@@ -4,17 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum = 0;
-	int x;
-	int i;
+	int x, y; //두개의 입력 정수 
+	char op; //연산자 문자 
+	int result; //연산 결과 저장 
 	
-	printf("input a number : ");
-	scanf("%d", &x);
-	
-	//for 문을 활용해서 1부터 x까지 sum에 더하는 코드
-	for (i=0;i<=x;i++) //반복(초기식;조건식;증감식) 
-	  sum = sum + i;  //더하기 
-	 
-	printf("더하기 결과는 %i입니다.\n", sum);  //결과 출력 
+	//두개의 입력정수, 한개의 연산자 문자 입력 받기 
+	printf("enter the calculation : ");
+	scanf ("%d %c %d", &x, &op, &y);
+
+   //연산자에 따라 결과값을 계산(result에 저장)
+   if (op == '+') //+인 경우
+       result = x + y;
+   else if (op == '-')//-인 경우
+       result = x - y;
+   else if (op == '*') // *인 경우
+       result = x * y;
+   else  // /인 경우
+       result = x / y;
+    
+   //printf로 출력 
+   printf("= %i\n", result);
 	return 0;
 }
